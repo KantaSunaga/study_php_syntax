04fc2b699937755168ac5b892fdd4beac3e8c237
KantaSunaga <sunagakannta6@gmail.com>
2017-07-28 07:32:42 +0900

    study calculate

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
  define("MY_EMAIL","hoge@hoge.com");
  echo MY_EMAIL;
+
+
+$x = 10%3; //1
+$y = 30.2/4; //7.55
+
+var_dump($x);
+var_dump($y);
+
+$z = 4;
+$z++;
+
+$a = 5;
+$a--;
+
+var_dump($z);
+var_dump($a);
